3c8823746c9a999f9af7920a81b5b8c9daaa2c71
Sebastian0698 <59987875+Sebastian0698@users.noreply.github.com>
2020-08-04 22:01:02 -0600

    Se realizo una prueba "Hola Mundo"

---
+++ backend/login/frontend/principal-vista.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TAXI | SS</title>
+    
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+    <link rel="stylesheet" href="css/style.css">
+
+    <style>
+        
+        body{
+            background-image: url(image/3inicio2.jpg); 
+
+            }
+    
+    </style>
+
+    
+</head>
+<body>
+   
+   <div class="welcome">
+       <img src="image/logo_magtimus.png" >
+        <h1>Bienvenido, somos SS</h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+   </div>
+   
+</body>
+</html>
